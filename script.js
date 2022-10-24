f727041a0cb302d0e7371c6ea2389d294533b306
nakyeonko3 <badaclock@gmail.com>
2022-10-24 23:44:51 +0900

    git init

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('hello, world!');
+const game_termainal = document.querySelector('.game-terminal');
+const start_button = document.querySelector('.start-button');
+
+function init(num) {
+  document.addEventListener('click', handleButtonClick);
+  function handleButtonClick(event) {
+    // event.preventDefault();
+    // game_termainal.innerHTML = `<p>게임 시작</p>`;
+    const p = document.createElement('p');
+    p.innerText = `게임을 시작하겠습니다.`;
+    const p2 = document.createElement('p');
+    p2.innerText = `게임을 시작하겠습니다.`;
+    fragment = document.createDocumentFragment();
+    fragment.appendChild(p2);
+    game_termainal.appendChild(fragment);
+  }
+}
+
+init(0);
