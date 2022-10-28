d1192d531d23e9ff126f1e4cc09aad2b75d3133d
nakyeonko3 <badaclock@gmail.com>
2022-10-29 02:08:15 +0900

    foreach, delete실습

--- 배열연습/script.js
+++ 배열연습/script.js
@@ -3,29 +3,35 @@ const submit_array = [];
 const form = document.querySelector('form'),
   input_text = form.querySelector('.input_text'),
   input_submit = form.querySelector('.input_submit'),
-  button = document.querySelector('button'),
+  deleteAll = document.querySelector('.deleteAll'),
   output = document.querySelector('.output');
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  submit_array.push(input_text.value);
-  input_text.value = '';
-});
-
-function printOutput(num) {
-  button.addEventListener('click', (event) => {
+form.addEventListener(
+  'submit',
+  (event) => {
     event.preventDefault();
+    submit_array.push(input_text.value);
+    const li = document.createElement('li');
+    li.innerText = input_text.value;
+    output.appendChild(li);
+    input_text.value = '';
+  },
+  true
+);
 
-    const fragment = document.createDocumentFragment();
-    submit_array.forEach((item) => {
-      const li = document.createElement('li');
-      li.id = ++num;
-      li.innerText = item;
-      fragment.appendChild(li);
+function deleteAllTodos() {
+  deleteAll.addEventListener('click', (event) => {
+    event.preventDefault();
+    submit_array.forEach((element) => {
+      document.querySelector('li').remove();
     });
-    output.appendChild(fragment);
-    button.remove();
+    document.querySelector('li').remove();
+    submit_array.splice(0, submit_array.length);
   });
 }
 
-printOutput(0);
+array.forEach((element) => {});
+
+document.addEventListener();
+
+deleteAllTodos();
