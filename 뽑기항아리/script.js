4f86d8f33fb76ebb46b2a279ad786f8c231eb5fd
nakyeonko3 <badaclock@gmail.com>
2022-10-29 20:37:30 +0900

    update

--- 뽑기항아리/script.js
+++ 뽑기항아리/script.js
@@ -1,18 +1,18 @@
 const iwantTODO = [
   ' 맛있는 거 치킨 피자 시켜 먹기',
-  '몬파라하기',
   '귀칼 유곽편 보기 아니면 다른 애니 봐도 됨',
+  '몬파라하기',
+  '긴나성 trpg, 옥냥이scp 유튜브 영상 보기',
   '침대에 누워서 노래 듣기',
   '마녀기사안나하기12',
   '마녀기사안나하기3',
   '덩킨 도넛 사먹기',
-  '긴나성 trpg, 옥냥이scp 유튜브 영상 보기',
   ' 다크리온 플레이 하기',
   'pixiv, e-hentai망가 야한거 탐닉하기',
 ];
 const button = document.querySelector('button'),
   span = document.querySelector('span');
-
+  
 for (let i = 0; i < 14; i++) {
   iwantTODO.push('더 열심히 웹개발 공부하기');
 }
@@ -20,6 +20,7 @@ for (let i = 0; i < 14; i++) {
 function mathRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
 
 button.addEventListener('click', (event) => {
   event.preventDefault();
