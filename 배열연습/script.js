24397f521e39d40b4a1d28bb1707a84b658ec5d4
nakyeonko3 <badaclock@gmail.com>
2022-10-31 21:17:21 +0900

    update > 배열연습/script.js, 전체 todo 지우기 기능 추가

--- 배열연습/script.js
+++ 배열연습/script.js
@@ -1,33 +1,40 @@
-const submit_array = [];
+const submitedArray = [];
 
 const form = document.querySelector('form'),
-  input_text = form.querySelector('.input_text'),
-  input_submit = form.querySelector('.input_submit'),
+  inputText = form.querySelector('.input_text'),
+  inputSubmit = form.querySelector('.input_submit'),
   deleteAll = document.querySelector('.deleteAll'),
   output = document.querySelector('.output');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  submit_array.push(input_text.value);
-  const li = document.createElement('li');
-  li.innerText = input_text.value;
-  output.appendChild(li);
-  input_text.value = '';
+  submitedArray.push(inputText.value);
+  createLiElement(inputText.value);
+  inputText.value = '';
 });
 
-function deleteAllTodos() {
+function createLiElement(value) {
+  const li = document.createElement('li');
+  li.innerText = value;
+  output.appendChild(li);
+}
+
+function deleteTodos() {
   deleteAll.addEventListener('click', (event) => {
     event.preventDefault();
-    submit_array.forEach((element) => {
-      document.querySelector('li').remove();
-    });
-    document.querySelector('li').remove();
-    submit_array.splice(0, submit_array.length);
+    removeLiTag();
+    spliceSubmitArray();
   });
 }
 
-array.forEach((element) => {});
+function spliceSubmitArray() {
+  submitedArray.splice(0, submitedArray.length);
+}
 
-document.addEventListener();
+function removeLiTag() {
+  submitedArray.forEach(() => {
+    document.querySelector('li').remove();
+  });
+}
 
-deleteAllTodos();
+deleteTodos();
